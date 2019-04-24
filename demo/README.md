71d1c5fd6f8c33ad478195ac5f68546b605dfa58
denvash <justdenva@gmail.com>
2019-04-24 15:03:56 +0300

    [~] Improve Markdown

---
+++ demo/README.md
@@ -0,0 +1,208 @@
+# [![Logo](/images/logo-github.png)](https://marketplace.visualstudio.com/items?itemName=DennisVash.summer-time#overview)
+
+[![Version](https://vsmarketplacebadge.apphb.com/version-short/DennisVash.summer-time.svg?subject=Summer%20Time&colorA=2B303B&colorB=A8F79A)](https://marketplace.visualstudio.com/items?itemName=DennisVash.summer-time) [![Rating](https://vsmarketplacebadge.apphb.com/rating/DennisVash.summer-time.svg?label=Ratings&colorA=2B303B&colorB=AEE9F5)](https://marketplace.visualstudio.com/items?itemName=DennisVash.summer-time) [![Installs](https://vsmarketplacebadge.apphb.com/installs/DennisVash.summer-time.svg?label=Installs&colorA=2B303B&colorB=F699D9)](https://marketplace.visualstudio.com/items?itemName=DennisVash.summer-time) [![Downloads](https://vsmarketplacebadge.apphb.com/downloads/DennisVash.summer-time.svg?label=Downloads&colorA=2B303B&colorB=EBEA8B)](https://marketplace.visualstudio.com/items?itemName=DennisVash.summer-time)
+
+> A modern Dark Material Theme which lets you forget the summer while coding.
+
+The **minimalistic design** makes the developer to **concentrate** on what's important while **understanding** the code flow.
+
+*Look it up at [VS-Marketplace](https://marketplace.visualstudio.com/items?itemName=DennisVash.summer-time#overview).*
+
+## Color Palette
+
+<div align="center">
+    <a>
+        <img src="https://user-images.githubusercontent.com/27515937/52947285-f4f36b00-337e-11e9-9d42-06537443c648.png" alt="Summer-Time-Logo" width="300px" />
+    </a>
+</div>
+
+## Usage
+
+- ![#F699D9](https://placehold.it/15/F699D9/000000?text=+) `#F699D9` Keywords
+- ![#AEE9F5](https://placehold.it/15/AEE9F5/000000?text=+) `#AEE9F5` Variables
+- ![#EBEA8B](https://placehold.it/15/EBEA8B/000000?text=+) `#EBEA8B` Operators
+- ![#A8F79A](https://placehold.it/15/A8F79A/000000?text=+) `#A8F79A` Static
+- ![#ECF6FF](https://placehold.it/15/ECF6FF/000000?text=+) `#ECF6FF` Functions
+- ![#282c34](https://placehold.it/15/282c34/000000?text=+) `#282c34` Background
+
+## Screenshots
+
+<div align="center">
+
+![JavaScript Syntax](https://img.shields.io/badge/SYNTAX-JavaScript-gray.svg?colorB=F699D9)
+![JavaScript](images/example-syntax-js.png)
+
+![hr](images/hr.png)
+
+![React Syntax](https://img.shields.io/badge/SYNTAX-React-gray.svg?colorB=F699D9)
+![hooks](/images/example-syntax-react-stories.png)
+
+![hr](images/hr.png)
+
+![react](images/example-syntax-react.png)
+
+![hr](images/hr.png)
+
+![example2](/images/example-syntax-redux-actions.png)
+
+![hr](images/hr.png)
+
+![Editor](https://img.shields.io/badge/EDITOR-View-gray.svg?colorB=F699D9)
+![example2](/images/example-editor-js.png)
+
+![hr](images/hr.png)
+
+![Editor](https://img.shields.io/badge/Syntax-JSON-gray.svg?colorB=F699D9)
+![example2](/images/example-syntax-json.png)
+
+![hr](images/hr.png)
+
+![Editor](https://img.shields.io/badge/Syntax-HTML-gray.svg?colorB=F699D9)
+![example2](/images/example-syntax-html.png)
+
+![hr](images/hr.png)
+
+![Editor](https://img.shields.io/badge/Syntax-Terminal-gray.svg?colorB=F699D9)
+![example2](/images/example-syntax-terminal.png)
+
+</div>
+
+## Configuration
+
+- FiraCode and Hack fonts, find more at [Nerd-Fonts](https://github.com/ryanoasis/nerd-fonts)
+- [Material Icon Theme](https://marketplace.visualstudio.com/items?itemName=DennisVash.summer-time)
+- [Bracket Pair Colorizer](https://marketplace.visualstudio.com/items?itemName=CoenraadS.bracket-pair-colorizer)
+
+Recommended [VSCode Settings](https://code.visualstudio.com/docs/getstarted/settings):
+
+```json
+// Work Bench
+"workbench.colorTheme": "Summer Time",
+"workbench.iconTheme": "material-icon-theme",
+"workbench.statusBar.feedback.visible": false,
+"workbench.statusBar.visible": true,
+"workbench.startupEditor": "newUntitledFile",
+"workbench.editor.highlightModifiedTabs": true,
+
+// Window
+"window.menuBarVisibility": "toggle",
+"window.zoomLevel": 1,
+"window.titleBarStyle": "custom",
+
+// Explorer
+"explorer.confirmDelete": false,
+"explorer.sortOrder": "type",
+"explorer.confirmDragAndDrop": false,
+"explorer.decorations.badges": false,
+
+// Editor
+"editor.fontFamily": "FuraCode Nerd Font, Light",
+"editor.mouseWheelZoom": true,
+"editor.snippetSuggestions": "bottom",
+"editor.autoClosingBrackets": "always",
+"editor.rulers": [100],
+"editor.renderIndentGuides": false,
+"editor.fontLigatures": true,
+"editor.renderControlCharacters": false,
+"editor.autoClosingQuotes": "always",
+"editor.formatOnSave": true,
+"editor.formatOnType": false,
+"editor.fontSize": 15,
+"editor.lineHeight": 24.65,
+"editor.letterSpacing": 0.5,
+"editor.fontWeight": "400",
+"editor.cursorStyle": "line-thin",
+"editor.cursorWidth": 5,
+"editor.cursorBlinking": "solid",
+"editor.renderWhitespace": "all",
+"editor.glyphMargin": true,
+"editor.suggest.snippetsPreventQuickSuggestions": true,
+
+// Terminal
+"terminal.integrated.cursorStyle": "underline",
+"terminal.integrated.cursorBlinking": true,
+"terminal.integrated.fontSize": 14,
+"terminal.integrated.fontWeight": "normal",
+"terminal.integrated.fontFamily": "Hack Nerd Font Mono, ExtraLight",
+"terminal.integrated.rendererType": "dom",
+"terminal.integrated.fontWeightBold": "normal",
+
+// Files
+"files.autoSave": "onFocusChange",
+"files.trimTrailingWhitespace": true,
+"files.trimFinalNewlines": true,
+"files.exclude": {
+  "**/.git": true,
+  "**/.svn": true,
+  "**/.hg": true,
+  "**/CVS": true,
+  "**/.DS_Store": true,
+  "**/node_modules": true
+},
+"files.associations": {
+  "*.react.js": "javascriptreact",
+  "*.stories.js": "javascriptreact",
+  "*.js": "javascript"
+},
+
+// Material Icons
+"material-icon-theme.activeIconPack": "react_redux",
+"material-icon-theme.folders.theme": "specific",
+"material-icon-theme.hidesExplorerArrows": true,
+"material-icon-theme.folders.color": "#F699D9",
+"material-icon-theme.files.associations": {
+  "*.ts": "typescript",
+  "fileName.ts": "angular",
+  "*.react.js": "react"
+},
+
+// Sync
+"sync.gist": "37489160d4242adee13d821d95858f97",
+"sync.autoUpload": true,
+"sync.autoDownload": true,
+"sync.quietSync": false,
+
+// Git
+"git.autofetch": true,
+"git.confirmSync": false,
+
+// Prettier
+"prettier.eslintIntegration": true,
+"prettier.singleQuote": true,
+
+// Eslint
+"eslint.autoFixOnSave": true,
+"eslint.enable": true,
+"eslint.run": "onType",
+
+// SOP's Import Cost Extension Settings.
+"importCost.largePackageColor": "#F699D9",
+"importCost.mediumPackageColor": "#EBEA8B",
+"importCost.smallPackageColor": "#EBEA8B",
+
+// Better comments
+"better-comments.tags": [
+  {
+    "tag": "*",
+    "color": "#EBEA8B",
+    "strikethrough": false,
+    "backgroundColor": "transparent"
+  }
+],
+
+// Bookmarks
+"numberedBookmarks.gutterIconFillColor": "#F699D9",
+"numberedBookmarks.gutterIconNumberColor": "#000",
+
+// Others
+"javascript.format.enable": false,
+"javascript.updateImportsOnFileMove.enabled": "always",
+"breadcrumbs.enabled": true,
+"debug.toolBarLocation": "docked",
+"markdown-preview-enhanced.enableTypographer": true,
+"svg.preview.autoOpen": true
+```
+
+## Other Places
+
+- [JetBrains-Theme](https://github.com/denvash/summer-time-theme-jetbrains)
--- demo/StoredPipesTable.react.js
+++ demo/StoredPipesTable.react.js
@@ -1,6 +1,19 @@
-
 import { connect } from 'react-redux';
-import { Table, Card, Button, Row, Col, Modal, Icon, Tag, Tooltip, Switch, Input, Popover, notification} from 'antd';
+import {
+  Table,
+  Card,
+  Button,
+  Row,
+  Col,
+  Modal,
+  Icon,
+  Tag,
+  Tooltip,
+  Switch,
+  Input,
+  Popover,
+  notification
+} from 'antd';
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import ReactJson from 'react-json-view';
@@ -9,18 +22,21 @@ import cronstrue from 'cronstrue';
 import { init } from '../../../actions/storedPipes.action';
 import { openModal } from '../../../actions/modal.action';
 import {
-   execStoredPipe,
-   deleteStoredPipeline,
-   updateStoredPipeline,
-   cronStart,
-   cronStop
-  } from '../../../actions/storedPipes.action';
+  execStoredPipe,
+  deleteStoredPipeline,
+  updateStoredPipeline,
+  cronStart,
+  cronStop
+} from '../../../actions/storedPipes.action';
 import './StoredPipesTable.scss';
 import HEditor from '../HEditor.react';
-import { RECORD_STATUS } from '../../../constants/colors'
-import { ReactComponent as PlayIconSvg } from '../../../images/play-icon.svg'
+import { RECORD_STATUS } from '../../../constants/colors';
+import { ReactComponent as PlayIconSvg } from '../../../images/play-icon.svg';
 
 const { Column } = Table;
+
+const generateNCards = N =>
+  [...Array(N)].map((_, i) => <Card key={`Card_${i}`}>Card-{i}</Card>);
 class StoredPipesTable extends Component {
   componentWillMount() {
     this.props.init();
@@ -33,38 +49,44 @@ class StoredPipesTable extends Component {
 
     // Need to remove "nodes" key from each pipeline.
     const fixedDataSource = [];
-    dataSource.forEach((p) => {
+    dataSource.forEach(p => {
       const pipe = JSON.parse(JSON.stringify(p));
       delete pipe.nodes;
       fixedDataSource.push(pipe);
     });
 
     const deleteConfirmAction = (action, record) => {
-      Modal.confirm(
-        {
-          title: 'WARNING Deleting Pipeline',
-          content: `Are you sure you want to delete ${record.name}? Deleting Pipeline will Stop-ALL related Jobs and Executions`,
-          okText: 'Confirm',
-          okType: 'danger',
-          cancelText: 'Cancel',
-          onOk() {
-            action(record.name);
-          },
-          onCancel() {}
+      Modal.confirm({
+        title: 'WARNING Deleting Pipeline',
+        content: `Are you sure you want to delete ${
+          record.name
+        }? Deleting Pipeline will Stop-ALL related Jobs and Executions`,
+        okText: 'Confirm',
+        okType: 'danger',
+        cancelText: 'Cancel',
+        onOk() {
+          action(record.name);
         },
-      );
+        onCancel() {}
+      });
     };
 
-    const revertCronTrigger = (record,cronStart,cronStop) => {
+    const revertCronTrigger = (record, cronStart, cronStop) => {
       const pipelineName = record.name;
-      const isCronEnabled = record.hasOwnProperty('triggers') && record.triggers.hasOwnProperty('cron');
-      return () => isCronEnabled ? ( record.triggers.cron.enabled ?
-        cronStop(pipelineName) : cronStart(pipelineName)) : {};
-    }
+      const isCronEnabled =
+        record.hasOwnProperty('triggers') &&
+        record.triggers.hasOwnProperty('cron');
+      return () =>
+        isCronEnabled
+          ? record.triggers.cron.enabled
+            ? cronStop(pipelineName)
+            : cronStart(pipelineName)
+          : {};
+    };
 
     const updateCronPattern = (pipeline, pattern, updateStoredPipeline) => {
       try {
-         cronstrue.toString(pattern);
+        cronstrue.toString(pattern);
         pipeline.triggers.cron.pattern = pattern;
         updateStoredPipeline(pipeline);
       } catch (errorMessage) {
@@ -74,19 +96,24 @@ class StoredPipesTable extends Component {
         notification.open({
           message: 'Cron Job Error',
           description: errorMessage,
-          icon: <Icon type="warning" style={{color:'red'}}/>
-        })
+          icon: <Icon type="warning" style={{ color: 'red' }} />
+        });
       }
-    }
+    };
 
     return (
       <div>
         <Table
           rowKey="name"
           dataSource={dataSource.asMutable()}
-          pagination={{ className: "tablePagination", defaultCurrent: 1, pageSize: 15, hideOnSinglePage: true,}}
+          pagination={{
+            className: 'tablePagination',
+            defaultCurrent: 1,
+            pageSize: 15,
+            hideOnSinglePage: true
+          }}
           locale={{ emptyText: 'No Stored Pipelines' }}
-          expandedRowRender={(record) => (
+          expandedRowRender={record => (
             <Card title="Full details">
               <ReactJson
                 src={record}
@@ -98,19 +125,21 @@ class StoredPipesTable extends Component {
                 enableClipboard={false}
               />
             </Card>
-          )}>
-          <Column
-            title="Pipeline Name"
-            dataIndex="name"
-            key="name"/>
+          )}
+        >
+          <Column title="Pipeline Name" dataIndex="name" key="name" />
           <Column
             title="Cron Job"
             dataIndex="cron"
             key="cron"
-            render={(_,record) => {
-              const cronIsEnabled = record.hasOwnProperty('triggers') &&
-                record.triggers.hasOwnProperty('cron') && record.triggers.cron.enabled
-              const cronExpr = cronIsEnabled ? record.triggers.cron.pattern : "* * * * *"
+            render={(_, record) => {
+              const cronIsEnabled =
+                record.hasOwnProperty('triggers') &&
+                record.triggers.hasOwnProperty('cron') &&
+                record.triggers.cron.enabled;
+              const cronExpr = cronIsEnabled
+                ? record.triggers.cron.pattern
+                : '* * * * *';
               return (
                 <Row type="flex" justify="start">
                   <Col span={4} order={1}>
@@ -118,24 +147,45 @@ class StoredPipesTable extends Component {
                       checkedChildren={<Icon type="check" />}
                       unCheckedChildren={<Icon type="close" />}
                       checked={cronIsEnabled}
-                      onChange={revertCronTrigger(record,this.props.cronStart,this.props.cronStop)}/>
+                      onChange={revertCronTrigger(
+                        record,
+                        this.props.cronStart,
+                        this.props.cronStop
+                      )}
+                    />
                   </Col>
                   <Col span={8} order={2}>
-                    <Popover content={cronExpr.split(' ').length===5 ? cronstrue.toString(cronExpr, {use24HourTimeFormat: true}) : 'Invalid Cron-Expression'} trigger="focus">
-                      <Input.Search className="cronInput"
+                    <Popover
+                      content={
+                        cronExpr.split(' ').length === 5
+                          ? cronstrue.toString(cronExpr, {
+                              use24HourTimeFormat: true
+                            })
+                          : 'Invalid Cron-Expression'
+                      }
+                      trigger="focus"
+                    >
+                      <Input.Search
+                        className="cronInput"
                         size="small"
                         disabled={!cronIsEnabled}
                         placeholder="Cron Expression"
-                        enterButton={<Icon type="check"/>}
+                        enterButton={<Icon type="check" />}
                         defaultValue={cronExpr}
-                        onSearch={pattern => updateCronPattern(JSON.parse(JSON.stringify(record)), pattern, this.props.updateStoredPipeline)}
-                    />
+                        onSearch={pattern =>
+                          updateCronPattern(
+                            JSON.parse(JSON.stringify(record)),
+                            pattern,
+                            this.props.updateStoredPipeline
+                          )
+                        }
+                      />
                     </Popover>
                   </Col>
                 </Row>
-            )}
-            }
-            />
+              );
+            }}
+          />
           <Column
             title="Status"
             dataIndex="status"
@@ -143,77 +193,96 @@ class StoredPipesTable extends Component {
             render={() => (
               <Row>
                 <Col className="align-center" span={2}>
-                  <Tooltip placement="top" title={"Creating"} >
+                  <Tooltip placement="top" title={'Creating'}>
                     <Tag color={RECORD_STATUS.creating}>{3}</Tag>
                   </Tooltip>
                 </Col>
                 <Col className="align-center" span={2}>
-                  <Tooltip placement="top" title={"Completed"} >
+                  <Tooltip placement="top" title={'Completed'}>
                     <Tag color={RECORD_STATUS.completed}>{3}</Tag>
                   </Tooltip>
                 </Col>
                 <Col className="align-center" span={2}>
-                  <Tooltip placement="top" title={"Active"} >
+                  <Tooltip placement="top" title={'Active'}>
                     <Tag color={RECORD_STATUS.active}>{3}</Tag>
                   </Tooltip>
                 </Col>
                 <Col className="align-center" span={2}>
-                  <Tooltip  placement="top" title={"Stopped"} >
-                    <Tag color={RECORD_STATUS.stopped }>{3}</Tag>
+                  <Tooltip placement="top" title={'Stopped'}>
+                    <Tag color={RECORD_STATUS.stopped}>{3}</Tag>
                   </Tooltip>
                 </Col>
                 <Col className="align-center" span={2}>
-                  <Tooltip className="toolTip" placement="top" title={"Failed"} >
-                    <Tag color={RECORD_STATUS.failed }>{3}</Tag>
+                  <Tooltip className="toolTip" placement="top" title={'Failed'}>
+                    <Tag color={RECORD_STATUS.failed}>{3}</Tag>
                   </Tooltip>
                 </Col>
                 <Col className="align-center" span={2}>
-                  <Tooltip className="toolTip" placement="top" title={"Skipped"} >
-                    <Tag color={RECORD_STATUS.skipped }>{3}</Tag>
+                  <Tooltip
+                    className="toolTip"
+                    placement="top"
+                    title={'Skipped'}
+                  >
+                    <Tag color={RECORD_STATUS.skipped}>{3}</Tag>
                   </Tooltip>
                 </Col>
               </Row>
-            )
-            }
-            />
+            )}
+          />
           <Column
             title="Action"
             dataIndex="action"
             key="action"
-            render={((_,record) => (
+            render={(_, record) => (
               <Row type="flex" justify="start">
                 <Col span={4}>
                   <HEditor
-                    jsonTemplate={JSON.stringify(fixedDataSource.find((p) => p.name === record.name), null, 2)}
-                    styledButton={(onClick) =>
+                    jsonTemplate={JSON.stringify(
+                      fixedDataSource.find(p => p.name === record.name),
+                      null,
+                      2
+                    )}
+                    styledButton={onClick => (
                       <Button shape="circle" onClick={onClick}>
-                        <Icon component={PlayIconSvg}/>
+                        <Icon component={PlayIconSvg} />
                       </Button>
-                    }
+                    )}
                     title={'Execute Pipeline Editor'}
                     okText={'Execute'}
                     action={this.props.execStoredPipe}
                   />
                 </Col>
-                <Col span={4} >
+                <Col span={4}>
                   <HEditor
-                    jsonTemplate={JSON.stringify(dataSource.find((p) => p.name === record.name), null, 2)}
-                    styledButton={(onClick) =>
-                      <Button shape="circle" icon="edit" onClick={onClick}/>
-                    }
+                    jsonTemplate={JSON.stringify(
+                      dataSource.find(p => p.name === record.name),
+                      null,
+                      2
+                    )}
+                    styledButton={onClick => (
+                      <Button shape="circle" icon="edit" onClick={onClick} />
+                    )}
                     title={'Edit Pipeline Editor'}
                     okText={'Update'}
                     action={this.props.updateStoredPipeline}
                   />
                 </Col>
-                <Col span={4} >
-                  <Button type="danger" shape="circle" icon="delete"
-                    onClick={() => deleteConfirmAction(this.props.deleteStoredPipeline, record)}
+                <Col span={4}>
+                  <Button
+                    type="danger"
+                    shape="circle"
+                    icon="delete"
+                    onClick={() =>
+                      deleteConfirmAction(
+                        this.props.deleteStoredPipeline,
+                        record
+                      )
+                    }
                   />
                 </Col>
               </Row>
-            ))}
-            />
+            )}
+          />
         </Table>
       </div>
     );
@@ -227,14 +296,15 @@ StoredPipesTable.propTypes = {
   deleteStoredPipeline: PropTypes.func.isRequired,
   updateStoredPipeline: PropTypes.func.isRequired,
   cronStop: PropTypes.func.isRequired,
-  cronStart: PropTypes.func.isRequired,
+  cronStart: PropTypes.func.isRequired
 };
 
-const mapStateToProps = (state) => ({
+const mapStateToProps = state => ({
   dataSource: state.storedPipeline.dataSource
 });
 
-export default connect(mapStateToProps,
+export default connect(
+  mapStateToProps,
   {
     openModal,
     init,
@@ -243,4 +313,5 @@ export default connect(mapStateToProps,
     updateStoredPipeline,
     cronStop,
     cronStart
-  })(StoredPipesTable);
+  }
+)(StoredPipesTable);
