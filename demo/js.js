366702f77985a64b90e8e28b6eeb546e0b73061b
denvash <justdenva@gmail.com>
2019-04-29 18:24:00 +0300

    [~] Comment fix

--- demo/js.js
+++ demo/js.js
@@ -1,40 +1,40 @@
-'use strict'
+'use strict';
 class Sale {
   constructor(price) {
-    ;[this.decoratorsList, this.price] = [[], price]
+    [this.decoratorsList, this.price] = [[], price];
   }
 
   decorate(decorator) {
-    if (!Sale[decorator]) throw new Error(`decorator not exist: ${decorator}`)
-    this.decoratorsList.push(Sale[decorator])
+    if (!Sale[decorator]) throw new Error(`decorator not exist: ${decorator}`);
+    this.decoratorsList.push(Sale[decorator]);
   }
 
   getPrice() {
     for (let decorator of this.decoratorsList) {
-      this.price = decorator(this.price)
+      this.price = decorator(this.price);
     }
-    return this.price.toFixed(2)
+    return this.price.toFixed(2);
   }
 
   static quebec(price) {
     // this is a comment
-    return price + price * 7.5 / 100
+    return price + (price * 7.5) / 100;
   }
 
   static fedtax(price) {
-    return price + price * 5 / 100
+    return price + (price * 5) / 100;
   }
 }
 
-let sale = new Sale(100)
+let sale = new Sale(100);
 
-sale.decorate('fedtax')
-sale.decorate('quebec')
-console.log(sale.getPrice()) //112.88
+sale.decorate('fedtax');
+sale.decorate('quebec');
+console.log(sale.getPrice()); //112.88
 
-getPrice()
+getPrice();
 
-const arr =[1,2,3];
+const arr = [1, 2, 3];
 arr.forEach(console.log);
 
 //deeply nested
@@ -43,15 +43,25 @@ async function asyncCall() {
   var result = await resolveAfter2Seconds();
 }
 
-for (let i=0; i <10; i++) {
+for (let i = 0; i < 10; i++) {
   continue;
 }
 
-if (true) {}
+const hello = hi;
+
+let maxDepth = out.reduce((max, curr) => {
+  const root = hello;
+  const depth = root;
+  const [depth] = curr;
+  return max >= depth ? max : depth;
+}, 0);
+
+if (true) {
+}
 
 while (true) {}
 
-switch(2) {
+switch (2) {
   case 2:
     break;
   default:
