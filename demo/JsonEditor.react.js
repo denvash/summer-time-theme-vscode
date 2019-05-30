4237b4da6631a78f9683074107bb57193338dc7b
denvash <justdenva@gmail.com>
2019-05-30 17:51:14 +0300

    [~] Enchance Const coloring

---
+++ demo/JsonEditor.react.js
@@ -0,0 +1,29 @@
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
+
+import MonacoEditor from 'react-monaco-editor';
+
+export default function JsonEditor({ width, height, ...props }) {
+  const [value, setValue] = useState(props.value);
+  return (
+    <MonacoEditor
+      {...props}
+      width={width || 800}
+      height={height || 600}
+      language="json"
+      value={value}
+      onChange={data => {
+        setValue(data);
+        props.onChange && props.onChange(data);
+      }}
+    />
+  );
+}
+
+JsonEditor.propTypes = {
+  onChange: PropTypes.func,
+  value: PropTypes.string.isRequired,
+  snippetEnabled: PropTypes.bool,
+  showGutter: PropTypes.bool,
+  style: PropTypes.object
+};
