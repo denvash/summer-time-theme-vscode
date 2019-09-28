64adb1cdbb997e4f8178c880d63d44c8215d9a58
Dennis Vash <denvash@gmail.com>
2019-09-28 18:42:05 +0300

    [~] Fix url color in styled-component

--- demo/statelessfunctionalreact.js
+++ demo/statelessfunctionalreact.js
@@ -1,3 +1,15 @@
+const GlobalStyle = createGlobalStyle`
+  @font-face {
+    font-family: 'Calibre';
+    src: url(${fontFamilies.CalibreLightWOFF2}) format('woff2'),
+    url(${fontFamilies.CalibreLightWOFF}) format('woff'),
+    url(${fontFamilies.CalibreLightTTF}) format('truetype');
+    font-weight: 300;
+    font-style: normal;
+    font-display: auto;
+  }
+`;
+
 const StyledIcon = styled(Icon)`
   svg {
     height: calc(${({ dimension }) => dimension || '24px'} + 3vw);
@@ -6,23 +18,27 @@ const StyledIcon = styled(Icon)`
 `;
 
 // I use this syntax when my component fits on one line
-const ListItem = props => <li className="list-item">{props.item.name}</li>
+const ListItem = props => <li className="list-item">{props.item.name}</li>;
 
 // I use this when my component has no logic outside JSX
 const List = ({ items }) => (
-  <ul className="list">{items.map(item => <ListItem item={item} />)}</ul>
-)
+  <ul className="list">
+    {items.map(item => (
+      <ListItem item={item} />
+    ))}
+  </ul>
+);
 
 // I use this when the component needs logic outside JSX.
 const Body = props => {
-  let items = transformItems(props.rawItems)
+  let items = transformItems(props.rawItems);
   return (
     <div>
       <h1>{props.header}</h1>
       <List items={items} />
     </div>
-  )
-}
+  );
+};
 
 // This is equivalent to the last example
 function Page(props, context) {
@@ -30,9 +46,9 @@ function Page(props, context) {
     <div>
       <Body header="My List" rawItems={props.rawItems} />
     </div>
-  )
+  );
 }
 // propTypes and contextTypes are supported
 Page.propTypes = {
   rawItems: React.PropTypes.array.isRequired
-}
+};
